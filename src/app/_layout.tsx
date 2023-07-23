d79aef071ad0019944d049cc8c05554ffddff361
Lukas Lindqvist <mail@lukaslindqvist.se>
2023-07-23 20:46:42 +0200

    Project setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -42,7 +42,7 @@ function RootLayoutNav() {
     <>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="/" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
       </ThemeProvider>
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "../components/Themed";
+
+export default function Root() {
+
+  return (
+  <View>
+    <Text>Root</Text>
+  </View>
+  )
+}
